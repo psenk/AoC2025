39d059de643c63b15833ed7d7088e23671fa2f29
Tyler <psenk168@gmail.com>
2025-12-10 15:02:35 -0600

    day one completed

--- day_one.py
+++ day_one.py
@@ -1,4 +1,52 @@
+instructions = []
 with open('data\\day_one.txt') as file:
 
     for line in file:
-        print(line)
+        ins = (line[0], int(line[1:].strip()))
+        instructions.append(ins)
+
+"""
+STAR ONE
+
+dial_point = 50
+zero_count = 0
+
+for rotation in instructions:
+    if rotation[0] == 'L':
+        dial_point -= rotation[1]
+        if dial_point < 0:
+            dial_point %= 100
+    else:
+        dial_point += rotation[1]
+        if dial_point > 99:
+            dial_point %= 100
+
+    if dial_point == 0:
+        zero_count += 1
+
+print('Zero Count: ', zero_count)
+ """
+
+dial_point = 50
+zero_count = 0
+
+for direction, amount in instructions:
+
+    if direction == 'L':
+        while amount > 0:
+            if dial_point == 0:
+                zero_count += 1
+            amount -= 1
+            dial_point -= 1
+            if dial_point < 0:
+                dial_point = 99
+    else:
+        while amount > 0:
+            if dial_point == 0:
+                zero_count += 1
+            amount -= 1
+            dial_point += 1
+            if dial_point > 99:
+                dial_point = 0
+
+print('Zero Count: ', zero_count)
